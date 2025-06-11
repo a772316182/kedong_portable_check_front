95cb7374b00f3c1b66b1b529b98552e6deb6746e
haoran <772316182@qq.com>
2025-06-11 15:53:24 +0800

    feat(layout): 添加用户信息展示及操作面板
    - 在顶部栏添加用户头像和用户名
    - 实现用户信息下拉菜单，包含个人资料、修改密码、注销账户和退出登录等功能
    - 新增 username 变量用于存储用户名信息

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const leftDrawerOpen = ref(false)
+const username = ref('张三')
 </script>
 
 
@@ -15,6 +16,27 @@ const leftDrawerOpen = ref(false)
           </q-avatar>
           便携式检查工具
         </q-toolbar-title>
+        <q-space/>
+        <q-avatar>
+          <Icon name="material-symbols:person-check" size="35" mode="svg"/>
+        </q-avatar>
+        <q-btn-dropdown stretch flat :label="`欢迎：${username}`">
+          <q-list>
+            <q-item-label header>操作面板</q-item-label>
+            <q-item>
+              <q-btn flat class="full-width">个人资料</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="full-width">修改密码</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="full-width">注销账户</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="full-width">退出登录</q-btn>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
 
