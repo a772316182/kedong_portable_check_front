5c328b6122de3a7d24000747543427e0b66a7a46
suncen242 <2285344126@qq.com>
2025-07-03 16:22:55 +0800

    新增隔离策略详情dialog

--- components/strategy_check/strategy_check_verification_isolation.vue
+++ components/strategy_check/strategy_check_verification_isolation.vue
@@ -1,42 +1,45 @@
 <script setup lang="ts">
 const selectedRows = ref([])
 
+const showPolicyDialog = ref(false);
+const currentDevice = ref(null);
+
 const devices = ref([
   {
-    index: 1, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 1, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 2, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 2, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 3, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 3, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 4, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 4, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 5, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 5, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 6, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 6, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 7, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 7, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 8, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 8, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 9, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 9, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 10, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 10, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 11, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 11, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   },
   {
-    index: 12, name: '山东省调', ip: '10.200.114.23', type: '纵向', zone: 'Ⅰ区', count: '1'
+    index: 12, name: '山东省调', ip: '10.200.114.23', type: '隔离', zone: 'Ⅰ区', count: '1'
   }
 ])
 
@@ -47,6 +50,11 @@ const columns = {
   type: '设备类型',
   zone: '安全区',
   count: '策略数量'
+}
+
+function handlePolicyClick(name: any) {
+  currentDevice.value = name;
+  showPolicyDialog.value = true;
 }
 
 
@@ -68,8 +76,13 @@ const columns = {
         <template #top-right>
           <q-btn label="策略导入" color="primary"/>
         </template>
+
+        <template #cell-count="{ row }">
+          <q-btn outline color="primary" @click="handlePolicyClick(row)">{{ row.count }}</q-btn>
+        </template>
       </common-enhanced-table>
     </q-card-section>
   </q-card>
+  <strategy-check-verification-isolation-dialog v-model="showPolicyDialog" :name="currentDevice"/>
 </template>
 
